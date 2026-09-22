e3511dd48033a7eeb1b22bc85f075de0b75e54e9
Mind0t <irving.soriano31@gmail.com>
2026-09-21 21:02:43 -0600

    Agregue el calculo de maximo y minimo a analisis.py

--- src/analisis.py
+++ src/analisis.py
@@ -3,7 +3,11 @@ datos=[10,20,30,40,50]
 suma_datos=sum(datos)
 num_datos=len(datos)
 prom=suma_datos/num_datos
+max_datos=max(datos)
+min_datos=min(datos)
 print(f"Estos son los valores del arreglo {datos}")
 print(f"Esta es la suma de esos datos {suma_datos}")
 print(f"Es la cantidad de datos del arreglo {num_datos}")
 print(f"Este es el promedio de los datos {prom}")   
+print(f"Este es el maximo de los datos {max_datos}")
+print(f"Este es el minimo de los datos {min_datos}")
