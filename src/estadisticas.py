cb0cdbe41d9a6521d6d69192a221a94bb48bb06d
Mind0t <irving.soriano31@gmail.com>
2026-09-21 21:52:24 -0600

    Agregar script de estadisticas descriptivas

---
+++ src/estadisticas.py
@@ -0,0 +1,5 @@
+import statistics
+datos=[10,20,30,40,50,60,70]
+print(f"Media: {statistics.mean(datos)}")
+print(f"Mediana: {statistics.median(datos)}")
+print(f"Desviacion: {statistics.stdev(datos):.2f}")
