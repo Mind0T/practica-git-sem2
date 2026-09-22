77b01362e7a473a06960bbe5d42b49f63bd10f71
Mind0t <irving.soriano31@gmail.com>
2026-09-21 20:57:35 -0600

    creacion analisis.py y calculo de promedio

---
+++ src/analisis.py
@@ -0,0 +1,9 @@
+#Esto es un repason y creacion de un calculador de promedio
+datos=[10,20,30,40,50]
+suma_datos=sum(datos)
+num_datos=len(datos)
+prom=suma_datos/num_datos
+print(f"Estos son los valores del arreglo {datos}")
+print(f"Esta es la suma de esos datos {suma_datos}")
+print(f"Es la cantidad de datos del arreglo {num_datos}")
+print(f"Este es el promedio de los datos {prom}")   
